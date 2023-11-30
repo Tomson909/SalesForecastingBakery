47c327de15d38ebd18db0013245b2c38e511e74d
Tomson909 <tom.ruge@me.com>
2023-11-30 15:11:19 +0100

    do stuff

--- 0_DataPreparation/Toms_dataprep.R
+++ 0_DataPreparation/Toms_dataprep.R
@@ -18,20 +18,29 @@ setwd("/home/tomruge/Schreibtisch/UNI/Semester_7/Einführung_in_datascience_masc
 umsatzdaten_gekuerzt <- read_csv("umsatzdaten_gekuerzt.csv", col_types = cols(Datum = col_date(format = "%Y-%m-%d"), Warengruppe = col_integer(), Umsatz = col_double()))
 kiwo <- read_csv("kiwo.csv", col_types = cols(Datum = col_date(format = "%Y-%m-%d"), KielerWoche = col_integer()))
 wetter <- read_csv("wetter.csv", col_types = cols(Datum = col_date(format = "%Y-%m-%d"), Bewoelkung = col_integer(), Temperatur = col_double(), Windgeschwindigkeit = col_integer(), Wettercode = col_integer()))
-feiertage <- read_csv("Feiertage_Deutschland.csv", col_types = cols(Summaryy = col_character(), Description = col_character(), Datum = col_date(format = "%Y-%m-%d")))
+feiertage <- read_csv("Feiertage_Deutschland.csv", col_types = cols(Summary = col_character(), Description = col_character(), Datum = col_date(format = "%Y-%m-%d")))
+ferien <- read_csv('ferien.csv', col_types = cols(Datum = col_date(format = "%Y-%m-%d"), IsFerien = col_logical()))
 
-# ferien müssen noch hinzugefügt werden
-
+# feiertage logical
+feiertage <- feiertage %>%
+  mutate(IsFeiertag = TRUE) %>%
+  select(Datum, IsFeiertag)
 
 # datensätze zusammenführen
 dataset <- umsatzdaten_gekuerzt %>%
   full_join(kiwo, by = "Datum") %>%
   full_join(wetter, by = "Datum") %>%
-  full_join(feiertage, by = "Datum")
+  full_join(feiertage, by = "Datum") %>%
+  full_join(ferien, by = "Datum")
 
-print(dataset)
-
-
+# map numeric values to categorical values
+dataset <- dataset %>%
+  mutate(Produktname = case_when(Warengruppe == 1 ~ "Brot",
+                                 Warengruppe == 2 ~ "Broetchen",
+                                 Warengruppe == 3 ~ "Croissant",
+                                 Warengruppe == 4 ~ "Konditorei",
+                                 Warengruppe == 5 ~ "Kuchen",
+                                 Warengruppe == 6 ~ "Saisonbrot"))
 
 # Define the proportions for training, validation, and test sets
 train_prop <- 0.6
